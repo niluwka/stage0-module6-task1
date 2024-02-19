dd6da3be55f4aab78b3692e97db219f9db812ea2
nilufar <ninimamatova0403@gmail.com>
2024-02-19 11:21:01 +0500

    Animals

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,36 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
+    private String color;
+    private  int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+
+
+
+    }
+
+    public    String  getDescription( ){
+
+        if (numberOfPaws != 1 ) {
+            if (hasFur == true) {
+                return "This animal is mostly " +    color + ". It has "+ numberOfPaws + " paws and a fur.";
+            }else {
+                return "This animal is mostly " +    color + ". It has "+ numberOfPaws + " paws and no fur.";
+            }
+
+        }else {
+            if (hasFur == true){
+                return "This animal is mostly " + color + ". It has "+ numberOfPaws + " paw and a fur.";
+            }else {
+                return "This animal is mostly " +    color + ". It has "+ numberOfPaws + " paw and no fur.";
+            }
+
+        }
+
+    }
 }
